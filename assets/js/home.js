cd7c0ddb1b79edeb2828cdef2bea3fdf708224ec
tuanduong1907 <91052899+tuanduong1907@users.noreply.github.com>
2022-01-09 01:29:43 +0700

    Update home.js

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,27 +1,28 @@
-$(document).ready(function(){
-  $('.container__slider').slick({
-      slidesToShow: 1,
-      // autoplay: true,
-      autoplaySpeed: 2000,
-      arrows: false,
-      dots: true,
-
-      responsive: [
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 2
+window.addEventListener('load', function() {
+  $(document).ready(function(){
+    $('.container__slider').slick({
+        slidesToShow: 1,
+        // autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        dots: true,
+  
+        responsive: [
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
             }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          }
-        ]
-  });
-});
-
+          ]
+    });
+  });  
+})
